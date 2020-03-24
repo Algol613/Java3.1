9590ffe2ef6246754875652f14e27c625617e309
Svetlana Bodnar <ss.bodnar@yandex.ru>
2020-03-24 23:54:45 +0300

    programm added

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        float cost = 11725;
+        float mile = (float) Math.floor(cost/20);
+
+        System.out.println(mile);
+
+    }
+}
