4039e0f9170dfedfbfbf449ab1bba11c3ff085f9
Svetlana Bodnar <ss.bodnar@yandex.ru>
2020-03-25 02:01:35 +0300

    add basic interface

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
-        float cost = 11725;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Ticet coast: ");
+        float cost = in.nextFloat();
         float mile = (float) Math.floor(cost/20);
-
-        System.out.println(mile);
+        in.close();
+        System.out.printf("Added bonus miles: %s \n", mile);
 
     }
 }
